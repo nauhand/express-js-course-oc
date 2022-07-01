9ac739782e572524f300fb77151f28134b94a2e3
Dev Naunau <olivier.nauhand@gmail.com>
2022-07-01 05:55:23 +0000

    up

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 var cors = require('cors')
 const app = express();
 
-mongoose.connect('mongodb+srv://nauhand:parental0011@cluster0.47erv.mongodb.net/?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+mongoose.connect('mongodb+srv://nauhand:parental0011@cluster0.47erv.mongodb.net/?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use(express.json());
 
 app.use(cors());
 
 app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
