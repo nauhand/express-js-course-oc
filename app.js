763a2f9a92d479d41a238624fcfdde58e8863343
Franck Olivier Nauhand <nauhand00@gmail.com>
2022-07-01 12:42:42 +0000

    up

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const stuffRoutes = require('./routes/stuff');
 const userRoutes = require('./routes/user');
+const path = require('path');
 
 var cors = require('cors')
+
 const app = express();
 
 mongoose.connect('mongodb+srv://nauhand:parental0011@cluster0.47erv.mongodb.net/?retryWrites=true&w=majority', {
@@ -17,6 +19,7 @@ app.use(express.json());
 
 app.use(cors());
 
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/stuff', stuffRoutes);
 app.use('/api/auth', userRoutes);
 
